6fc51ef4be7d467aea1c8a3c6a085413e37392f9
VostaMartinVM <74717642+VostaMartinVM@users.noreply.github.com>
2020-12-01 16:21:56 +0100

    Added Task class

--- src/Deadline.java
+++ src/Deadline.java
@@ -1,3 +1,68 @@
 public class Deadline
 {
+    private int day;
+    private int month;
+    private int year;
+
+    public Deadline (int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * Changes the day month and year with given argument.
+     * @param obj this is the deadline that we are replacing the old one with.
+     */
+
+    public void changeDeadline (Deadline obj)
+    {
+        this.day = obj.day;
+        this.month = obj.month;
+        this.year = obj.year;
+    }
+
+    public boolean equals (Object obj)
+    {
+        if (!(obj instanceof Deadline))
+        {
+            return false;
+        }
+
+        Deadline newDeadline = (Deadline)obj;
+
+        return day == newDeadline.day &&
+                month == newDeadline.month &&
+                year == newDeadline.year;
+    }
+
+    public String toString()
+    {
+        return "The deadline is: " + day + ":" + month + ":" + year;
+    }
 }
---
+++ src/Task.java
@@ -0,0 +1,114 @@
+public class Task
+{
+    private String name;
+    private int ID;
+    private String description;
+    private int estimatedTime;
+    private String status;
+    private int hoursWorked;
+    private Deadline deadline;
+    private Employee employee;
+
+
+    public Task (String name, int ID, String description, int estimatedTime, String status, int hoursWorked, Deadline deadline, Employee employee)
+    {
+        this.name = name;
+        this.ID = ID;
+        this.description = description;
+        this.estimatedTime = estimatedTime;
+        this.status = status;
+        this.hoursWorked = hoursWorked;
+        this.deadline = deadline;
+        this.employee = null;
+    }
+
+    /**
+     * This constructor will set the status to a default one.
+     */
+
+    public Task (String name, int ID, String description, int estimatedTime, int hoursWorked, Deadline deadline, Employee employee)
+    {
+        this.name = name;
+        this.ID = ID;
+        this.description = description;
+        this.estimatedTime = estimatedTime;
+        this.hoursWorked = hoursWorked;
+        this.deadline = deadline;
+        this.employee = null;
+        status = "Not Started";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setEstimatedTime(int estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public boolean equals (Object obj)
+    {
+        if (!(obj instanceof Task))
+        {
+            return false;
+        }
+
+        Task newTask = (Task)obj;
+
+        return name == newTask.name &&
+                ID == newTask.ID &&
+                description == newTask.description &&
+                estimatedTime == newTask.estimatedTime &&
+                status == newTask.status &&
+                hoursWorked == newTask.hoursWorked &&
+                deadline == newTask.deadline;
+    }
+
+    public void assignEmployee(Employee employee)
+    {
+        if (!(this.employee.equals(employee) ))
+        {
+            this.employee = employee;
+        }
+    }
+}
