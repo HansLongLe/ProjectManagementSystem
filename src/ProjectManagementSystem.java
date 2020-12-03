0fdca40a8b0c8e393320554eee40c8d252f8bdcb
asold2 <74716450+asold2@users.noreply.github.com>
2020-12-03 15:24:15 +0100

    TeamMember, Employee, ProjectManagementSystem, Project commit

--- src/ProjectManagementSystem.java
+++ src/ProjectManagementSystem.java
@@ -9,21 +9,39 @@ public  class ProjectManagementSystem
     employees = new ArrayList<Employee>();
     projects = new ArrayList<Project>();
   }
+  /*
+  adding projects to project managemnt system
+   */
   public void addProject(Project project){
     projects.add(project);
   }
+  /*
+  removing proh=jects from project management system
+   */
   public void removeProject(Project project){
     projects.remove(project);
   }
+  /*
+  adding employees to project management system
+   */
   public void addEmployee(Employee employee){
     employees.add(employee);
   }
+  /*
+  removing emplpoyees from project management system
+   */
   public void removeEmployee(Employee employee){
     employees.remove(employee);
   }
+  /*
+  returning an array list of projects
+   */
   public ArrayList<Project> getProjects(){
     return projects;
   }
+  /*
+  returning an array list of employees
+   */
   public ArrayList<Employee> getEmployees(){
     return employees;
   }
