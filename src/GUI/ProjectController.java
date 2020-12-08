9aebf542fd300f91bac93f4ff312229a9e3a14de
asold2 <74716450+asold2@users.noreply.github.com>
2020-12-08 13:27:47 +0100

    fxml file

--- src/GUI/ProjectController.java
+++ src/GUI/ProjectController.java
@@ -8,73 +8,76 @@ import javafx.event.ActionEvent;
 
 public class ProjectController
 {
-//  @FXML private Tab projects;
-//  @FXML private Tab projectInfo;
-//  @FXML private Tab requirements;
-//  @FXML private Tab requirementsInfo;
-//  @FXML private Tab tasks;
-//  @FXML private Tab taskInfo;
-//  @FXML private TabPane tabPane;
-//
-//  @FXML private ToggleButton position;
-//  @FXML private MenuItem scrumMaster;
-//  @FXML private MenuItem projectCreator;
-//  @FXML private MenuItem teamMember;
-//  @FXML private MenuItem about;
-//
-//  @FXML private Button addProject;
-//  @FXML private Button deleteProject;
-//  @FXML private Button addRequirement;
-//  @FXML private Button deleteRequirement;
-//  @FXML private Button addTask;
-//  @FXML private Button deleteTask;
-//
-//  @FXML private TextField projectName;
-//  @FXML private TextField projectID;
-//  @FXML private TextField productOwner;
-//  @FXML private TextField projectEstimatedTime;
-//  @FXML private TextField projectDeadline;
-//  @FXML private TextField projectDescription;
-//  @FXML private ComboBox projectStatus;
-//
-//
-//  @FXML private TextField requirementName;
-//  @FXML private TextField requirementID;
-//  @FXML private ToggleButton requirementPriority ;
-//  @FXML private RadioButton priority1;
-//  @FXML private RadioButton priority2;
-//  @FXML private RadioButton priority3;
-//  @FXML private TextField requirementEstimatedTime;
-//  @FXML private TextField requirementDeadline;
-//  @FXML private TextField requirementDescription;
-//  @FXML private ComboBox requirementStatus;
-//
-//  @FXML private TextField taskName;
-//  @FXML private TextField taskID;
-//  @FXML private TextField respTeamMember;
-//  @FXML private TextField taskEstimatedTime;
-//  @FXML private TextField taskDeadline;
-//  @FXML private TextField taskDescription;
-//  @FXML private TextField taskHoursWorked;
-//  @FXML private ComboBox taskStatus;
-//
-//  @FXML private Button taskSave;
-//  @FXML private Button taskChange;
-//  @FXML private Button requirementSave;
-//  @FXML private Button requirementChange;
-//  @FXML private Button projectSave;
-//  @FXML private Button projectChange;
-//
-//  @FXML private ListView projectList;
-//  @FXML private ListView requirementList;
-//  @FXML private ListView taskList;
+  @FXML private Tab projects;
+  @FXML private Tab projectInfo;
+  @FXML private Tab requirements;
+  @FXML private Tab requirementsInfo;
+  @FXML private Tab tasks;
+  @FXML private Tab taskInfo;
+  @FXML private TabPane tabPane;
+
+  @FXML private ToggleButton position;
+  @FXML private MenuItem scrumMaster;
+  @FXML private MenuItem projectCreator;
+  @FXML private MenuItem teamMember;
+  @FXML private MenuItem about;
+
+  @FXML private Button addProject;
+  @FXML private Button deleteProject;
+  @FXML private Button addRequirement;
+  @FXML private Button deleteRequirement;
+  @FXML private Button addTask;
+  @FXML private Button deleteTask;
+
+  @FXML private TextField projectName;
+  @FXML private TextField projectID;
+  @FXML private TextField productOwner;
+  @FXML private TextField projectEstimatedTime;
+  @FXML private TextField projectDeadline;
+  @FXML private TextField projectDescription;
+  @FXML private ComboBox projectStatus;
 
 
-//  public void handleActions(ActionEvent e){
-//    if(e.getSource() == addProject){
-//      tabPane.getSelectionModel().select(projectInfo);
-//    }
-//  }
+  @FXML private TextField requirementName;
+  @FXML private TextField requirementID;
+  @FXML private ToggleButton requirementPriority ;
+  @FXML private RadioButton priority1;
+  @FXML private RadioButton priority2;
+  @FXML private RadioButton priority3;
+  @FXML private TextField requirementEstimatedTime;
+  @FXML private TextField requirementDeadline;
+  @FXML private TextField requirementDescription;
+  @FXML private ComboBox requirementStatus;
+
+  @FXML private TextField taskName;
+  @FXML private TextField taskID;
+  @FXML private TextField respTeamMember;
+  @FXML private TextField taskEstimatedTime;
+  @FXML private TextField taskDeadline;
+  @FXML private TextField taskDescription;
+  @FXML private TextField taskHoursWorked;
+  @FXML private ComboBox taskStatus;
+
+  @FXML private Button taskSave;
+  @FXML private Button taskChange;
+  @FXML private Button requirementSave;
+  @FXML private Button requirementChange;
+  @FXML private Button projectSave;
+  @FXML private Button projectChange;
+
+  @FXML private ListView projectList;
+  @FXML private ListView requirementList;
+  @FXML private ListView taskList;
+
+
+
+
+  public void handleActions(ActionEvent e){
+    if(e.getSource() == addProject && position.equals(projectCreator)){
+      projectName.setDisable(false);
+      tabPane.getSelectionModel().select(projectInfo);
+    }
+  }
 
 
 
